abcc4c4ea85f48e4a4b4347f77fba1bd4930862f
OsirisJr16 <mintsanavalonajunior@example.com>
2025-05-17 14:06:49 +0300

    add:dist

---
+++ dist/assets/Cube-found-ChOUz20m.js
@@ -0,0 +1,1 @@
+const s="/assets/Cube-found-CvyEM_sY.png";export{s as S};
---
+++ dist/assets/index-BM1TxODq.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-i9ws2I_8.js";const o=()=>t.jsx("div",{className:"main-content bg-abstract-background overflow-y-auto sm:h-full sm:w-full",children:t.jsx(s,{})});export{o as default};
